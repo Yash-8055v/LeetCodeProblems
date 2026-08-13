f8c11aeb5d657df4cd4a7326723f4899bb7f567d
Yash Karande <yashkarande4240@gmail.com>
2026-08-13 23:14:17 +0530

    485: Max Consecutive Ones

---
+++ Prob_485.java
@@ -0,0 +1,26 @@
+
+// 485: Max Consecutive Ones
+// TC: O(n)
+
+public class Prob_485 {
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int maxCount = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                count++;
+                maxCount = Math.max(maxCount, count); 
+            } else {
+                count = 0; 
+            }  
+        }
+
+        return maxCount;
+    }
+    public static void main(String[] args) {
+        int nums[] = {1,1,0,1,1,1,0};
+		System.out.println("Max consicutive ones " + findMaxConsecutiveOnes(nums));
+
+    }
+}
