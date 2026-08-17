0ac7a42b57516e3fa1d48df8b3e544e58d40dc2a
Yash Karande <yashkarande4240@gmail.com>
2026-08-17 22:21:55 +0530

    283: Move Zeroes

---
+++ Prob_283.java
@@ -0,0 +1,27 @@
+
+// 283: Move Zeroes
+// TC: O(n)
+
+public class Prob_283 {
+    public static void moveZeroes(int[] nums) {
+        int j = 0; // tracks the position for next non zero elem
+    
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {0,1,0,3,12};
+        moveZeroes(nums);
+        for(int num : nums){
+            System.out.print(num + " ");
+        }
+    }
+}
